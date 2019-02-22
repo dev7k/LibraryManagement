dd071021fc89a8d010c0ae66d6d5b1ea2ae53730
dev7k <dev7k@protonmail.com>
2019-02-22 21:02:24 +0100

    added customer model

---
+++ src/Library/Data/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Required, MinLength(3), MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
