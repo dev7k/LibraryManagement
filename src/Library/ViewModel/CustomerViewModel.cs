edfa772f4c9fe0482e633b2aff0383bd4a615ded
dev7k <dev7k@protonmail.com>
2019-02-22 23:19:59 +0100

    added customer view model

---
+++ src/Library/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Library.Data.Model;
+
+namespace Library.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public int BookCount { get; set; }
+    }
+}
