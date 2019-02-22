0f1d0a71dd6eef30b9b7bd9da7885dbb4f7e2e82
dev7k <dev7k@protonmail.com>
2019-02-22 21:02:00 +0100

    added author model

---
+++ src/Library/Data/Model/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Model
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        [Required, MinLength(3), MaxLength(50)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
