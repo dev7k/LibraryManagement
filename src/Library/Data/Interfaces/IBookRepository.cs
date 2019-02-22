3a5e2efea5c9bf7d5c01e83a25f79fccce042926
dev7k <dev7k@protonmail.com>
2019-02-22 21:28:23 +0100

    added book repository interface

---
+++ src/Library/Data/Interfaces/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Data.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Data.Interfaces
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetAllWithAuthor();
+        IEnumerable<Book> FindWithAuthor(Func<Book, bool> predicate);
+        IEnumerable<Book> FindWithAuthorAndBorrower(Func<Book, bool> predicate);
+    }
+}
