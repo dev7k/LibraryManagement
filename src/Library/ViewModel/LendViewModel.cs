1909d90ec1d77526387d011ee44ba17639edf107
dev7k <dev7k@protonmail.com>
2019-02-23 13:51:59 +0100

    added lend view model

---
+++ src/Library/ViewModel/LendViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Library.Data.Model;
+using System.Collections.Generic;
+
+namespace Library.ViewModel
+{
+    public class LendViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
