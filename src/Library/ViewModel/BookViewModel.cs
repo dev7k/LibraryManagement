8869d2758ee3b0525c3eaecdf1b2423243c7990d
dev7k <dev7k@protonmail.com>
2019-02-23 11:48:18 +0100

    added book view model

---
+++ src/Library/ViewModel/BookViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Library.Data.Model;
+using System.Collections.Generic;
+
+namespace Library.ViewModel
+{
+    public class BookViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Author> Authors { get; set; }
+    }
+}
